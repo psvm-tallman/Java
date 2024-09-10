2d2405b0d0b2d9698dfa0c942549336ea8b56b3c
codewtallman <mehulsavio@gmail.com>
2024-09-10 17:15:57 +0530

    commit

---
+++ Arrays/Array.java
@@ -0,0 +1,12 @@
+package Arrays;
+
+public class Array {
+    public static void main(String[] args) {
+        int rol1 = 23;
+        int rol2 = 16;
+
+        int[] rol = new int[2];
+        
+
+    }
+}
